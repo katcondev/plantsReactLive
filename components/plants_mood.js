54a0122bf95869ef50a47b46286414e663b0219d
Kat Contreras <kat.contreras@gmail.com>
2022-12-31 19:47:20 -0800

    updated cards

--- components/plants_mood.js
+++ components/plants_mood.js
@@ -6,12 +6,12 @@ export default function PlantDB() {
     let [plants, setPlants] = React.useState('');
 
     const fetchData = React.useCallback(() => {
-      const requestUrl = "https://api.airtable.com/v0/appFj2Envr91ItMKQ/Plants";
+      const requestUrl = "https://api.airtable.com/v0/appUEHCG0RnYm88pm/plant-mood";
 
       fetch(requestUrl, {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer keykEQO9bMo2Fukln`,
+        Authorization: `Bearer keyrnJ3ystlKHJbYv`,
       },
   })
       .then((res) => res.json())
@@ -27,20 +27,18 @@ export default function PlantDB() {
     fetchData()
   }, [fetchData])
   return (
-<div className="flex cols-2">
+<div className="flex flex-row">
     {plants.length > 0 ? (
       plants.map((plant) => (
         <div key={plant.id} className="rounded overflow-hidden shadow-lg transform transition duration-300 hover:scale-105">
-                <div>
-                    <Image key={plant.id} src={plant.fields.Image[0].url} width={plant.fields.Image[0].width} height={plant.fields.Image[0].height} layout="responsive" alt="list of plants" />
-                </div>
+                
                 <div className="px-6 py-4">
                   <div key={plant.id} className="font-bold text-xl mb-2">Name: {plant.fields.Name}</div>
                     <p className="text-gray-700 text-base">
                    Personality: {plant.fields.Personality}
                     </p>
                     <p className="text-gray-700 text-base">
-                   Image: {plant.fields.Image[0].filename}
+                   Image: {plant.fields.Image[i].filename}
                     </p>
                 </div>
                 <div key={plant.id} className="px-6 pb-5">
